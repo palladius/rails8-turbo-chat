2b09f9e3b2fbe2d886794c32bd55e27138c62f23
a-chris <a.christian.toscano@gmail.com>
2025-09-17 22:23:53 +0200

    move device blacklise to devise initializer

--- app/config/initializers/devise.rb
+++ app/config/initializers/devise.rb
@@ -311,3 +311,9 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 end
+
+# Inspired by
+# https://github.com/heartcombo/devise/blob/354df3bc65fb211f0704208f52bf2213758585cf/lib/devise/models/authenticatable.rb#L59
+# https://stackoverflow.com/questions/53595363/how-to-avoid-accidentally-sending-sensitive-fields-from-my-model
+
+Devise::Models::Authenticatable::BLACKLIST_FOR_SERIALIZATION << :gemini_api_key
