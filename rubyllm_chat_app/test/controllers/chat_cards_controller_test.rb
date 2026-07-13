0a7904fb3f36b445667cbfde2d9daef506ba70a7
Riccardo Carlesso <ricc+chezmoi@google.com>
2026-07-13 19:00:03 +0200

    test: allow www.example.com host in tests to fix 403 Forbidden 🐛

--- rubyllm_chat_app/test/controllers/chat_cards_controller_test.rb
+++ rubyllm_chat_app/test/controllers/chat_cards_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class ChatCardsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
+    sign_in users(:one)
     get root_url
     assert_response :success
   end
